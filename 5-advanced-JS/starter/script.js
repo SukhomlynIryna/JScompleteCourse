50ad819c4215497c5816345fcc0712fb8e355edf
sukhomlyniryna <sukhomlyniryna@gmail.com>
2018-12-08 15:50:54 +0100

    Section5: callback functions

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -87,4 +87,34 @@
 
 var years = [1990, 1965, 1937, 2005, 1998];
 
-function arrayCalc(arr, fn ){}
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arr.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calcAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+
+    if (el >= 18 && <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years, calcAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(rates);
