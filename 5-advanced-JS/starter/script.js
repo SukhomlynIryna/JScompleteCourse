a398ca18b28b438ae2577caabd54985e97e37b4b
sukhomlyniryna <sukhomlyniryna@gmail.com>
2018-12-08 16:15:35 +0100

    Section5: functions returning functions

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -85,36 +85,62 @@
 
 // passing functions as arguments
 
-var years = [1990, 1965, 1937, 2005, 1998];
+//var years = [1990, 1965, 1937, 2005, 1998];
+//
+//function arrayCalc(arr, fn) {
+//    var arrRes = [];
+//    for (var i = 0; i < arr.length; i++) {
+//        arr.push(fn(arr[i]));
+//    }
+//    return arrRes;
+//}
+//
+//function calcAge(el) {
+//    return 2016 - el;
+//}
+//
+//function isFullAge(el) {
+//    return el >= 18;
+//}
+//
+//function maxHeartRate(el) {
+//
+//    if (el >= 18 && <= 81) {
+//        return Math.round(206.9 - (0.67 * el));
+//    } else {
+//        return -1;
+//    }
+//}
+//
+//var ages = arrayCalc(years, calcAge);
+//var fullAges = arrayCalc(ages, isFullAge);
+//var rates = arrayCalc(ages, maxHeartRate);
+//
+//console.log(ages);
+//console.log(rates);
 
-function arrayCalc(arr, fn) {
-    var arrRes = [];
-    for (var i = 0; i < arr.length; i++) {
-        arr.push(fn(arr[i]));
-    }
-    return arrRes;
-}
+// Functions returning functions
 
-function calcAge(el) {
-    return 2016 - el;
-}
-
-function isFullAge(el) {
-    return el >= 18;
-}
-
-function maxHeartRate(el) {
-
-    if (el >= 18 && <= 81) {
-        return Math.round(206.9 - (0.67 * el));
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function (name) {
+            console.log(name + ',can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function (name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
     } else {
-        return -1;
+        return function (name) {
+            console.log('Hello ' + name + ', what do you do ?');
+        }
     }
 }
 
-var ages = arrayCalc(years, calcAge);
-var fullAges = arrayCalc(ages, isFullAge);
-var rates = arrayCalc(ages, maxHeartRate);
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
 
-console.log(ages);
-console.log(rates);
+teacherQuestion('John');
+designerQuestion('John');
+
+interviewQuestion('teacher')('Mark');
